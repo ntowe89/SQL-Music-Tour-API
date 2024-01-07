4a1b3f3e9d74587371cdafe88ea446a444472c18
Nathan Towe <nathanwtowe89@gmail.com>
2024-01-06 19:52:51 -0500

    finished controllers

---
+++ controllers/stages_controller.js
@@ -0,0 +1,81 @@
+const stages = require('express').Router()
+const db = require('../models')
+const { Stage } = db
+const { Op } = require('sequelize')
+
+stages.get('/', async (req, res) => {
+    try {
+        const foundStage = await Stage.findAll({
+            where: {
+                stage_name: { [Op.like]: `%${req.query.stage_name ? req.query.stage_name : ''}`}
+            }
+        })
+        res.status(200).json(foundStage)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//SHOW
+stages.get('/:id', async (req, res) => {
+    try {
+        const foundStage = await Stage.findOne({
+            where: { stage_id: req.params.id }
+        })
+        res.status(200).json(foundStage)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//CREATE
+stages.post('/', async (req, res) => {
+    try {
+        const newStage = await Stage.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new stage',
+            data: newStage
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//UPDATE
+stages.put('/:id', async (req, res) => {
+    try {
+        const updatedStages = await Stage.update(req.body, {
+            where: {
+                stage_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedStages} stage(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//DELETE
+stages.delete('/:id', async (req, res) => {
+    try {
+        const deletedStages = await Stage.destroy({
+            where: {
+                stage_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedStages} stage(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
+
+
+module.exports = stages
+
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,6 +16,17 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+//controllers
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
